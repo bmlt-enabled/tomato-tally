b43ec72cc52869246744b4744bdd4726e26db4c3
pjaudiomv <pjaudiomv@gmail.com>
2018-10-14 21:52:20 -0400

    table view button

--- map.php
+++ map.php
@@ -30,14 +30,49 @@ $unique_coordinates = array_unique($coordinates, SORT_REGULAR);
 <body>
 <div id="map"></div>
 <script>
+    var map;
+    function CenterControl(controlDiv, map) {
 
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to go to table view';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Table View';
+        controlUI.appendChild(controlText);
+        controlUI.addEventListener('click', function() {
+            window.location.href = './index.php';
+        });
+
+    }
     function initMap() {
-
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 3,
             center: {lat: -28.024, lng: 140.887}
         });
 
+        var centerControlDiv = document.createElement('div');
+        var centerControl = new CenterControl(centerControlDiv, map);
+
+        // Create the DIV to hold the control and call the CenterControl()
+        // constructor passing in this DIV.
+        centerControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
         // Create an array of alphabetical characters used to label the markers.
         var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
